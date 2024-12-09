fb0563965077481aac8ec2312af2c6dccf14d3eb
loannesdev <zsevaxt@gmail.com>
2024-12-09 17:14:19 -0500

    feat: vite configuration added (including dev enviroment execution config)

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-const { invoke } = window.__TAURI__.core;
+import { setupCounter } from './counter.js'
+import javascriptLogo from './javascript.svg'
+import './style.css'
+import viteLogo from '/vite.svg'
 
-let greetInputEl;
-let greetMsgEl;
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite! Holi mundo</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+setupCounter(document.querySelector('#counter'))
