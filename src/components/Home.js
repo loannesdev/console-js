ea5505d9af12a93fb9bc6c31c2b8bf35dde67d3f
loannesdev <zsevaxt@gmail.com>
2024-12-18 18:52:22 -0500

    feat: architechture added

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+class Home extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  static get styles() {
+    return /* css */`
+      h1 {
+        color: red;
+      }
+    `
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = /* html */`
+    <style>${Home.styles}</style>
+
+    <h1>Hello World!</h1>
+  `
+  }
+}
+
+customElements.define('home-component', Home)
